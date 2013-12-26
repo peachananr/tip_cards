534117f78b3a6452de0775d92241a2c9c10328f8
peachananr <peachananr@gmail.com>
2013-12-26 13:45:59 +0700

    Initial release

---
+++ Demo/jquery.tip_cards.js
@@ -0,0 +1,271 @@
+/* ===========================================================
+ * jquery-tip_cards.js v1
+ * ===========================================================
+ * Copyright 2013 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Create a card layout that let your 
+ * user flip through it like you see on 
+ * https://www.google.com/get/googletips/
+ *
+ * https://github.com/peachananr/tip_cards
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+    entrance: "bottom",
+    column: 4,
+    margin: "1%",
+    selector: "> li",
+    hoverTilt: "right",
+    triggerSelector: "> li a",
+    cardFlyDirection: "all",
+    closeButton: "X",
+    flipButton: "Flip",
+    navigation: true,
+    beforeOpen: null,
+    afterOpen: null
+	};
+	
+	
+  
+  $.fn.tip_cards = function(options){
+    var settings = $.extend({}, defaults, options),
+        el = $(this);  
+    el.addClass("tc_body");
+    
+    $.each(el.find(settings.selector), function(i) {
+      $(this).addClass("tc_card").hide().css({
+        "width": (100/settings.column) - (parseInt(settings.margin) * 2) + "%",
+        "margin": settings.margin
+      }).wrapInner("<div class='tc_inner_card tilt_"+settings.hoverTilt+"'></div>");
+      if ($(this).find(".tc_front").length > 0) {
+        $(this).find(".tc_inner_card").addClass("tc_flipped");
+      }
+    });
+    
+    
+    setTimeout(function(){
+      $.each(el.find(".tc_card"), function(i) {
+        var current = $(this);
+        current.addClass("animate tc_entrance_" + settings.entrance).show();
+        current.find(".tc_inner_card").prepend("<span class='tc_shadow'></span>");
+        
+        if (current.find(".tc_front").length > 0) {
+          setTimeout(function(){
+            current.find(".tc_inner_card").removeClass("tc_flipped")
+          }  , 600);
+        }
+      });
+    }, 100);
+    
+    
+    function resetNav() {
+      if ($(".tc_card.active").prev(".tc_card").length < 1) {
+        $(".tc_modal_open .tc_prev").addClass("tc_entrance");
+      } else {
+        $(".tc_modal_open .tc_prev").removeClass("tc_entrance");
+      }
+      if ($(".tc_card.active").next(".tc_card").length < 1) {
+        $(".tc_modal_open .tc_next").addClass("tc_entrance");
+      } else {
+        $(".tc_modal_open .tc_next").removeClass("tc_entrance");
+      }
+    }
+    
+    function realignCards() {
+      var timeout = 100;
+
+      $(".tc_fake1").addClass("tc_straighten").css({
+        "position":"absolute",
+        "top": (( $(window).height() - $(".tc_modal").height() ) / 2+$(window).scrollTop()) + 16 + "px",
+        "left": ( $(window).width() - $(".tc_modal").width() ) / 2+$(window).scrollLeft() + "px",
+        "-webkit-transform": "scale(0.96)",
+        "-moz-transform": "scale(0.96)",
+        "-o-transform": "scale(0.96)",
+        "transform": "scale(0.96)"
+      });
+      setTimeout(function(){
+        $(".tc_fake2").addClass("tc_straighten").css({
+          "position":"absolute",
+          "top": (( $(window).height() - $(".tc_modal").height() ) / 2+$(window).scrollTop()) + 12 + "px",
+          "left": ( $(window).width() - $(".tc_modal").width() ) / 2+$(window).scrollLeft() + "px",
+          "-webkit-transform": "scale(0.97)",
+          "-moz-transform": "scale(0.97)",
+          "-o-transform": "scale(0.97)",
+          "transform": "scale(0.97)"
+        });
+        setTimeout(function(){
+          $(".tc_fake3").addClass("tc_straighten").css({
+            "position":"absolute",
+            "top": (( $(window).height() - $(".tc_modal").height() ) / 2+$(window).scrollTop()) + 8 + "px",
+            "left": ( $(window).width() - $(".tc_modal").width() ) / 2+$(window).scrollLeft() + "px",
+            "-webkit-transform": "scale(0.98)",
+            "-moz-transform": "scale(0.98)",
+            "-o-transform": "scale(0.98)",
+            "transform": "scale(0.98)"
+          });
+          setTimeout(function(){
+            $(".tc_fake4").addClass("tc_straighten").css({
+              "position":"absolute",
+              "top": (( $(window).height() - $(".tc_modal").height() ) / 2+$(window).scrollTop()) + 4 + "px",
+              "left": ( $(window).width() - $(".tc_modal").width() ) / 2+$(window).scrollLeft() + "px",
+              "-webkit-transform": "scale(0.99)",
+              "-moz-transform": "scale(0.99)",
+              "-o-transform": "scale(0.99)",
+              "transform": "scale(0.99)",
+            });
+            setTimeout(function(){
+              $(".tc_main_modal").css({
+                "position":"absolute",
+                "top": ( $(window).height() - $(".tc_modal").height() ) / 2+$(window).scrollTop() + "px",
+                "left": ( $(window).width() - $(".tc_modal").width() ) / 2+$(window).scrollLeft() + "px",
+                "bottom": "auto",
+                "right": "auto",
+                "visibility": "visible"
+              });
+
+              setTimeout(function(){
+                $(".tc_main_modal .tc_inner_modal").removeClass("tc_flipped");
+                if (typeof settings.afterOpen == 'function') settings.afterOpen();
+                resetNav();
+              }, 500);  
+            }, timeout);
+          }, timeout);
+        }, timeout);
+      }, timeout);
+    }
+    
+    $(window).resize(function() {
+      realignCards();
+    });
+    
+    el.find(settings.triggerSelector).addClass("tc_click_target").click(function() {
+      var el2 = $(this),
+      container = $(el2.attr("href"));
+      
+      
+      
+      el2.closest(".tc_card").addClass("active")
+      
+      if (typeof settings.beforeOpen == 'function') settings.beforeOpen();
+      
+      $("body").addClass("tc_modal_open");
+      $("<div class='tc_overlay'></div>").appendTo("body");
+      
+      
+      $("<div class='tc_modal tc_main_modal'><div class='tc_inner_modal'></div></div>").appendTo("body");
+      
+      
+      $(".tc_main_modal .tc_inner_modal").html(container.html())
+      
+      
+      if ($(".tc_main_modal .tc_front").length > 0) {
+        $(".tc_main_modal .tc_inner_modal").addClass("tc_flipped");
+      }
+      
+      if (settings.cardFlyDirection == "all") {
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake1 tc_to_top").attr("style", "").insertBefore(".tc_main_modal");
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake2 tc_to_bottom").attr("style", "").insertBefore(".tc_main_modal");
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake3 tc_to_left").attr("style", "").insertBefore(".tc_main_modal");
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake4 tc_to_right").attr("style", "").insertBefore(".tc_main_modal");
+      } else {
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake1 tc_to_" + settings.cardFlyDirection).attr("style", "").insertBefore(".tc_main_modal");
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake2 tc_to_" + settings.cardFlyDirection).attr("style", "").insertBefore(".tc_main_modal");
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake3 tc_to_" + settings.cardFlyDirection).attr("style", "").insertBefore(".tc_main_modal");
+        $(".tc_main_modal").clone( true ).removeClass("tc_main_modal").addClass("tc_fake4 tc_to_" + settings.cardFlyDirection).attr("style", "").insertBefore(".tc_main_modal");
+      }
+      
+     
+      addExtraButtons(settings);
+      
+      
+      realignCards();
+      
+      if (settings.navigation != false ) {
+        $("<a class='tc_entrance tc_next' href='#'>&raquo;</a>").insertAfter(".tc_overlay")
+        $("<a class='tc_entrance tc_prev' href='#'>&laquo;</a>").insertAfter(".tc_overlay");
+      }
+      
+      resetNav();
+      
+      
+      $(".tc_next").click(function() {
+        if ($(".tc_card.active").next(".tc_card").length > 0 && !$(this).hasClass("animated")) {
+          var el3 = $(this);
+          el3.addClass("animated");
+          var content = "<div class='tc_inner_modal'>" + $($(".tc_card.active").removeClass("active").next(".tc_card").addClass("active").find(".tc_click_target").attr("href")).html() + "</div>";
+          $(".tc_main_modal").addClass("old").clone().removeClass("old").html(content).insertBefore(".tc_main_modal.old")
+          addExtraButtons(settings);
+          $(".tc_main_modal.old").addClass("tc_to_bottom force").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+            $(".tc_main_modal.old.tc_to_bottom").remove();
+            
+            $(".tc_main_modal").removeClass("tc_flipped");
+            el3.removeClass("animated");
+            resetNav();
+          });
+        }
+      });
+      
+      $(".tc_prev").click(function() {
+        if ($(".tc_card.active").prev(".tc_card").length > 0 && !$(this).hasClass("animated")) {
+          var el3 = $(this);
+          el3.addClass("animated");
+          var content = "<div class='tc_inner_modal'>" + $($(".tc_card.active").removeClass("active").prev(".tc_card").addClass("active").find(".tc_click_target").attr("href")).html() + "</div>";
+          $(".tc_main_modal").addClass("old").clone().removeClass("old").addClass("tc_to_bottom force").html(content).insertAfter(".tc_main_modal.old")
+          setTimeout(function(){
+            $(".tc_main_modal.tc_to_bottom").removeClass("tc_to_bottom force")
+            $(".tc_main_modal:not(.old)").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+              $(".tc_main_modal.old").remove();
+              addExtraButtons(settings);
+              $(".tc_main_modal").removeClass("tc_flipped");
+              resetNav();
+              el3.removeClass("animated");
+            });
+          }, 100);
+          
+          return false;
+        }
+      });
+      
+      
+      function addExtraButtons(settings) {
+        if (settings.closeButton != false ) {
+          $(".tc_main_modal").prepend("<a class='tc_close' href='#'>" + settings.closeButton +"</a>");
+          $(".tc_main_modal .tc_flip_toggle").remove();
+        }
+        if (settings.flipButton != false && $(".tc_main_modal").find(".tc_front").length > 0 ) {
+          $(".tc_main_modal .tc_flip_toggle").remove();
+          $(".tc_main_modal").prepend("<a class='tc_flip_toggle' href='#'>" + settings.flipButton +"</a>");
+        }
+
+
+        $(".tc_overlay, .tc_close").click(function() {
+          $(".tc_overlay").fadeOut("slow", function() {
+            $(".tc_overlay").remove();
+          });
+          el2.closest(".tc_card").removeClass("active")
+          $(".tc_next, .tc_prev").addClass("tc_entrance")
+          $(".tc_next, .tc_prev").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+            $(".tc_next, .tc_prev").remove();
+          });
+          $(".tc_modal").attr("style","").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+            $(this).remove();
+            $("body").removeClass("tc_modal_open")
+          });
+          return false;
+        });
+
+        $(".tc_flip_toggle").click(function() {
+          $(".tc_main_modal .tc_inner_modal").toggleClass("tc_flipped");
+          return false;
+        });
+        
+      }
+      
+    });
+  }
+}(window.jQuery);
+
--- README.md
+++ README.md
@@ -1,4 +1,112 @@
-tip_cards
-=========
+#Tip Cards by Pete R.
+Create an animated card layout that let your viewer flip through it like you see on [Google Tips](https://www.google.com/get/googletips/) Page.
 
-Create a card layout that let your user flip through it like you see on Google Tips
+Created by [Pete R.](http://www.thepetedesign.com), Founder of [BucketListly](http://www.bucketlistly.com)
+
+[![Google Tip Cards](http://www.thepetedesign.com/images/tip_cards_image.png "Google Tip Cards")](http://www.thepetedesign.com/demos/tip_cards_demo.html)
+
+## Demo
+[View demo](http://www.thepetedesign.com/demos/tip_cards_demo.html)
+
+## Compatibility
+Modern browsers such as Chrome, Firefox, and Safari on  desktop have been tested. I have not tested this on IE.
+
+## Basic Usage
+Tip Cards plugin let you create a layout of cards with a card-like interaction that you see on [Google Tips](https://www.google.com/get/googletips/) Page. I've also added several new animation options so that you can personalize it to your own liking.
+
+To add this to your website, all you have to do is include the latest jQuery library together with `jquery.tip_cards.js` and `tip_cards.css` into your document's `<head>`, and follow the HTML markup as shown below:
+
+## 2 Sides Flippable HTML Markup
+
+This is the default markup if you are looking to replicate the effect you see on [Google Tips](https://www.google.com/get/googletips/) Page.
+  
+````html  
+<body>
+  ..
+  <ul class="tips">
+    <li>
+      <div class="tc_front">
+      	<a href="#tip1">...</a>
+      </div>
+      <div class="tc_back">...</div>
+      <div id="tip1" class="tip">
+        <div class="tc_front">...</div>
+        <div class="tc_back">...</div>
+      </div>
+    </li>
+  ..
+</body>
+````
+
+Note: If you would like the card to be flippable, the html markup above will work perfectly. The content inside `tc_front` will be the front side of the card and the content inside `tc_back` will be the back side of the card. 
+
+## Normal HTML Markup
+
+If you DO NOT want it to be flippable then take out all the `tc_front` and `tc_back` as shown below and your card will no longer be flippable.
+
+````html  
+<body>
+  ..
+  <ul class="tips">
+    <li>
+      <a href="#tip1">...</a>
+      <div id="tip1" class="tip">
+        ...
+      </div>
+    </li>
+  ..
+</body>
+````
+
+Once that is done, simply call the function like below and the plugin will automatically detect whether to make the card flippable or not from the markup you provided:
+
+````javascript
+  $(".tips").tip_cards({
+    entrance: "bottom", // This option let you determine the direction of the fly in entrance animation when all the cards appears. Available options are "bottom", "left", "right", and "top". The default value is "bottom".
+    column: 4, // The plugin also let you define how the card will be displayed and aligned. You can set the column of cards here. The default value is 4. 
+    margin: "1%", // You can define the margins between each cards here. Percentage is currently support at this point. The default is "1%".
+    selector: "> li", // You can define a custom selector if you do not want to use ul and li tags. This option accepts the normal CSS selector. The default value is "> li" 
+    hoverTilt: "right", // Define the tilt direction when cards are hovered here. Available options are "right", "left", "up", and "down". The default value is "right".
+    triggerSelector: "> li a", // You can also define a custom selector for the trigger button here. The default value is "> li a" which will use the link inside a list as a trigger to activate the card. 
+    cardFlyDirection: "all", // You can define the card fly animation when the modal appears here. Available options are "all", "top", "bottom", "left", and "right". The default value is "all" which will have the cards fly in from all direction and stack up under the opened modal
+    closeButton: "X", // You can define the content of the close button here. Change this to false to prevent the plugin from automatically generating the close button. The default string is "X".
+    flipButton: "Flip", // You can define the content of the flip button here. Change this to false to prevent the plugin from automatically generating the flip button. The default string is "Flip".
+    navigation: true, // Set this to true to allow users to navigate from one card to another when modal is opened. Change it to false to disable it. The default value is true.
+    beforeOpen: null, // A callback function that will be executed before the modal opens.
+    afterOpen: null // A callback function that will be executed after the modal opens.
+  });
+````
+
+
+## Callbacks
+
+You can use callbacks to perform actions before or after the modal opens.
+
+### beforeOpen()
+
+This callback will be called called before the modal opens after cards are clicked.
+
+````javascript
+$(".tips").tip_cards({
+  beforeOpen: function () {
+    ...
+  }
+});
+````
+
+### afterOpen()
+
+This callback will be called called after the modal opens.
+
+````javascript
+$(".tips").tip_cards({
+  afterOpen: function () {
+    ...
+  }
+});
+````
+
+Now, what you will have on your website is a gallery of cards that will let users interact in a way that is familiar and rich in experience like you see on [Google Tips](https://www.google.com/get/googletips/) Page. Thank you for dropping by and if you want to see more of my plugins, visit [The Pete Design](http://www.thepetedesign.com/#design), or follow me on [Twitter](http://www.twitter.com/peachananr) and [Github](http://www.github.com/peachananr).
+
+## Other Resources
+- Tutorial (Coming Soon)
